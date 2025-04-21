295851c29ffab198bed8e3fd75512dddd59bdf8c
yvgenchemeris <chemeris.lunat1k@gmail.com>
2025-04-20 17:27:42 -0700

    Delete endpoint added

--- src/main/java/com/ech/ff/featureflagmanager/controller/FeatureFlagController.java
+++ src/main/java/com/ech/ff/featureflagmanager/controller/FeatureFlagController.java
@@ -12,11 +12,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -72,5 +76,25 @@ public class FeatureFlagController {
             @Parameter(description = "Name of the environment", example = "production", required = true) 
             String envName) {
         return featureFlagRepository.getEnvFF(envName);
+    }
+
+    @DeleteMapping("/{envName}/{featureName}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Delete a feature flag", 
+               description = "Deletes the specified feature flag from the specified environment")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "204", description = "Feature flag deleted successfully"),
+        @ApiResponse(responseCode = "404", description = "Feature flag or environment not found"),
+        @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
+    public void deleteFeatureFlag(
+            @PathVariable("envName") 
+            @Parameter(description = "Name of the environment", example = "production", required = true)
+            String envName,
+            @PathVariable("featureName") 
+            @Parameter(description = "Name of the feature flag", example = "dark-mode", required = true)
+            String featureName) {
+        log.info("Deleting feature flag: {}, env: {}", featureName, envName);
+        featureFlagRepository.deleteFF(featureName, envName);
     }
 }
